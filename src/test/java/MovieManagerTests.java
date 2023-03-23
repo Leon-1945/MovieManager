db1d2a3248b47dafda681fe6805f67e3360a2e90
Leon1945 <Muravlevevgeniy@yandex.ru>
2023-03-23 20:29:43 +0400

    new commit 5

--- src/test/java/MovieManagerTests.java
+++ src/test/java/MovieManagerTests.java
@@ -64,4 +64,48 @@ public class MovieManagerTests {
 
         Assertions.assertArrayEquals(actual, expected);
     }
+    @Test
+    public void test5() {
+        MovieManager manager = new MovieManager(9);
+
+        manager.add("Film1");
+        manager.add("Film2");
+        manager.add("Film3");
+        manager.add("Film4");
+        manager.add("Film5");
+        manager.add("Film6");
+        manager.add("Film7");
+        manager.add("Film8");
+        manager.add("Film9");
+        manager.add("Film10");
+
+
+
+
+        String[] expected = {"Film10","Film9","Film8","Film7","Film6","Film5","Film4","Film3","Film2"};
+        String[] actual = manager.findlast();
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+    @Test
+    public void test6() {
+        MovieManager manager = new MovieManager(9);
+
+        manager.add("Film1");
+        manager.add("Film2");
+        manager.add("Film3");
+        manager.add("Film4");
+        manager.add("Film5");
+        manager.add("Film6");
+        manager.add("Film7");
+        manager.add("Film8");
+
+
+
+
+        String[] expected = {"Film8","Film7","Film6","Film5","Film4","Film3","Film2","Film1"};
+        String[] actual = manager.findlast();
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
 }
