fcd1736dd50be4c419dc22e5876a7e77b5a6ea75
Leon1945 <Muravlevevgeniy@yandex.ru>
2023-03-23 14:00:55 +0400

    new commit 3

--- src/test/java/MovieManagerTests.java
+++ src/test/java/MovieManagerTests.java
@@ -42,4 +42,26 @@ public class MovieManagerTests {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void test4() {
+        MovieManager manager = new MovieManager(11);
+
+        manager.add("Film1");
+        manager.add("Film2");
+        manager.add("Film3");
+        manager.add("Film4");
+        manager.add("Film5");
+        manager.add("Film6");
+        manager.add("Film7");
+        manager.add("Film8");
+        manager.add("Film9");
+        manager.add("Film10");
+        manager.add("Film11");
+
+
+        String[] expected = {"Film11","Film10","Film9","Film8","Film7","Film6","Film5","Film4","Film3","Film2","Film1"};
+        String[] actual = manager.findlast();
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
 }
